a691c0a333413b5a911b32d77277ce059cd3aad3
Dustch <ivnborgognoni@gmail.com>
2018-12-31 19:19:22 -0300

    fetching and displaying data

--- src/App.js
+++ src/App.js
@@ -13,18 +13,49 @@ import DisplayCard from './components/DisplayCard'
   border: 2px solid palevioletred;
   border-radius: 3px;
 `;*/
-
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      pic_url: ''
+    }
+  }
+
+  
+
+  componentWillMount(){
+    let url = `https://randomuser.me/api/`
+    fetch(url)
+    .then(res =>res.json())
+    .then((data)=>{
+      this.setState({
+        name: capitalizeFirstLetter(data.results[0].name.first) + " " + capitalizeFirstLetter(data.results[0].name.last),
+        email: data.results[0].email,
+        phone: data.results[0].cell,
+        address: data.results[0].location.street,
+        pic_url: data.results[0].picture.large,
+      })
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <DisplayCard 
-            photo_url="https://randomuser.me/api/portraits/men/82.jpg"
-            name="Roberto Dog"
-            email="example@gmail.com"
-            phone="5555555"
-            address="Fake st. 123"
+            photo_url={this.state.pic_url}
+            name={this.state.name}
+            email={this.state.email}
+            phone={this.state.phone}
+            address={this.state.address}
           />
         </header>
       </div>
